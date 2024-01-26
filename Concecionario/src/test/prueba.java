2260c24325f76c50c51968bc8e96d8ad40290d10
Jimmy Merino <44029363+jimmymerino50@users.noreply.github.com>
2024-01-26 09:37:44 +0100

    nuevo cambio del proyecto
    he probado un nuevo commit

---
+++ Concecionario/src/test/prueba.java
@@ -0,0 +1,11 @@
+package test;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("clase de prueba para git");
+		System.err.println("Otra prueba para subir a git");
+	}
+
+}
