8f21abd9e4faed86f799f42de5f76c92e2210da3
Jimmy Merino <44029363+jimmymerino50@users.noreply.github.com>
2024-01-26 09:40:46 +0100

    clase de prueba en util
    asdadada

---
+++ Concecionario/src/util/Prueba_util.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Prueba_util {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("clase de prueba en el paquete util");
+	}
+
+}
